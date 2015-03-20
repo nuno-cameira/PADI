a1d531c6a9f52b7aa584ff01c7c9e4bd5f8c4419
unknown <joaopnfernandes@gmail.com>
2015-03-20 21:37:35 +0000

    Added the User skeleton

---
+++ SharedModel/IMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padi.SharedModel
+{
+    public interface IMapper
+    {
+        IList<KeyValuePair<string, string>> Map(string fileLine);
+    }
+}
---
+++ User/UserApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padi.User
+{
+    public class UserApplication
+    {
+        public Client localClient;
+
+        public void Init( string EntryURL )
+        {
+            localClient = new Client( EntryURL );
+        }
+    }
+}
