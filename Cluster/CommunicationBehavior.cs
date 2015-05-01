493d54d7da1e2d578ec6d33612a18bed1029b342
nuno-cameira <cameiranuno@gmail.com>
2015-05-01 15:51:03 +0100

    partial commit of the strategy pattern (not working) just to switch pc

---
+++ Cluster/CommunicationBehavior.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padi.Cluster
+{
+
+    /*class CommunicationBehavior : ICommunicationBehavior 
+    {
+        public Node cluster;
+
+        public CommunicationBehavior(Node cluster)
+        {
+            this.cluster = cluster;
+        }
+    }*/
+
+
+    class FrozenCommunicationBehavior : ICommunicationBehavior
+    {
+        private bool tryPromote()
+        {
+            throw new SocketException();
+        }
+        public void promote()
+        {
+            throw new SocketException();
+        }
+        private void nodeAction(ClusterHandler onSucess, string url);
+        private void nodeAction(ClusterHandler onSucess, ClusterHandler onFail, string url);
+        private void clusterAction(ClusterHandler onSucess);
+        public void submit(int splits, byte[] mapper, string classname, string clientUrl);
+        public bool doWork(int split, byte[] code, string className, string clientUrl)
+        {
+            throw new SocketException();
+        }
+        public void join(string nodeUrl);
+        public void disconect(string peer);
+        //public void printStatus();
+        public void status();
+    }
+
+
+
+    class NormalCommunicationBehavior : ICommunicationBehavior
+    {
+        private bool tryPromote()
+        {
+            string lowestURL = this.URL;
+            bool wasPromoted = false;
+            foreach (KeyValuePair<string, INode> entry in cluster)
+            {
+                if (entry.Key.GetHashCode() < this.URL.GetHashCode())
+                {
+                    lowestURL = entry.Key;
+                    break;
+                }
+            }
+
+            //Chek if node is this or a remote one
+            if (lowestURL.Equals(this.URL))
+            {
+                promote();
+                wasPromoted = true;
+            }
+            else
+                nodeAction((node) => { node.promote(); }, lowestURL);
+
+            return wasPromoted;
+        }
+        public void promote()
+        {
+            throw new SocketException();
+        }
+        private void nodeAction(ClusterHandler onSucess, string url);
+        private void nodeAction(ClusterHandler onSucess, ClusterHandler onFail, string url);
+        private void clusterAction(ClusterHandler onSucess);
+        public void submit(int splits, byte[] mapper, string classname, string clientUrl);
+        public bool doWork(int split, byte[] code, string className, string clientUrl)
+        {
+            throw new SocketException();
+        }
+        public void join(string nodeUrl);
+        public void disconect(string peer);
+        //public void printStatus();
+        public void status();
+    }
+
+
+}
---
+++ Cluster/ICommunicationBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padi.Cluster
+{
+    public interface ICommunicationBehavior
+    {
+        bool tryPromote();
+        void promote();
+        //private void nodeAction(ClusterHandler onSucess, string url);
+        //private void nodeAction(ClusterHandler onSucess, ClusterHandler onFail, string url);
+        //private void clusterAction(ClusterHandler onSucess);
+        void submit(int splits, byte[] mapper, string classname, string clientUrl);
+        bool doWork(int split, byte[] code, string className, string clientUrl);
+        void join(string nodeUrl);
+        void disconect(string peer);
+        //public void printStatus();
+        void status();
+    }
+}
